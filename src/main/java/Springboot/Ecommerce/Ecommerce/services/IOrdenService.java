d3aa0bb0725509bd19727538bdedc623a283f1de
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2023-07-18 14:13:58 -0600

    Funcionalida de cantidad de ordenes

--- src/main/java/Springboot/Ecommerce/Ecommerce/services/IOrdenService.java
+++ src/main/java/Springboot/Ecommerce/Ecommerce/services/IOrdenService.java
@@ -2,7 +2,10 @@ package Springboot.Ecommerce.Ecommerce.services;
 
 import Springboot.Ecommerce.Ecommerce.models.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
 
     Orden save(Orden orden);
 }
